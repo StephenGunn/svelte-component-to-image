8b14bf27d33a1f4909e0933d7b611cc0b0fbfa3f
Stephen Gunn <stephengunn@gmail.com>
2024-11-08 12:33:25 -0600

    font experiments

--- src/routes/image/+server.ts
+++ src/routes/image/+server.ts
@@ -1,10 +1,10 @@
-import { error } from "@sveltejs/kit";
-import type { RequestHandler } from "./$types";
+import { error } from '@sveltejs/kit';
+import type { RequestHandler } from './$types';
 
 // import my test stuff
-import { image_from_component, type RenderOptions } from "$lib/index.js";
+import { image_from_component, type RenderOptions } from '$lib/index.js';
 
-import HelloWorld from "./HelloWorld.svelte";
+import HelloWorld from './HelloWorld.svelte';
 
 export const GET: RequestHandler = (async ({ url }) => {
   try {
@@ -12,24 +12,24 @@ export const GET: RequestHandler = (async ({ url }) => {
       width: 1200,
       height: 600,
       props: {
-        text: url.searchParams.get("text") ?? "text not found",
-        second: url.searchParams.get("second") ?? "text not found",
+        text: url.searchParams.get('text') ?? 'text not found',
+        second: url.searchParams.get('second') ?? 'text not found'
       },
       fonts: [
         {
-          name: "Typewriter",
-          url: `${url.origin}/TYPEWR__.TTF`,
+          name: 'Jost',
+          url: `${url.origin}/jost.woff`,
           weight: 400,
-          style: "normal",
-        },
-      ],
+          style: 'normal'
+        }
+      ]
     };
     const image = await image_from_component(HelloWorld, options);
     const response = new Response(image);
-    response.headers.append("content-type", "image/png");
+    response.headers.append('content-type', 'image/png');
     return response;
   } catch (e) {
     console.error(e);
-    error(500, "Error");
+    error(500, 'Error');
   }
 }) satisfies RequestHandler;
--- src/routes/image/HelloWorld.svelte
+++ src/routes/image/HelloWorld.svelte
@@ -1,49 +1,45 @@
 <script lang="ts">
-  interface Props {
-    text: string;
-    second: string;
-  }
+	interface Props {
+		text: string;
+		second: string;
+	}
 
-  let { text, second }: Props = $props();
+	let { text, second }: Props = $props();
 </script>
 
 <div id="container">
-  <h1>
-    {text} world!
-  </h1>
-  <div class="square">
-    {second}
-  </div>
+	<h1>
+		{text} world!
+	</h1>
+	<div class="square">
+		{second}
+	</div>
 </div>
 
 <style>
-  #container {
-    width: 1200px;
-    height: 600px;
-    display: flex;
-    align-items: center;
-    justify-content: center;
-    position: relative;
-    background: rgb(63, 94, 251);
-    background: radial-gradient(
-      circle,
-      rgba(63, 94, 251, 1) 0%,
-      rgba(252, 70, 107, 1) 100%
-    );
-  }
-  h1 {
-    color: red;
-    font-size: 75px;
-  }
-  .square {
-    position: absolute;
-    bottom: 1rem;
-    left: 1rem;
-    display: flex;
-    background: black;
-    color: white;
-    padding: 2rem;
-    font-size: 50px;
-  }
+	#container {
+		width: 1200px;
+		height: 600px;
+		display: flex;
+		font-family: 'Jost';
+		align-items: center;
+		justify-content: center;
+		position: relative;
+		background: rgb(63, 94, 251);
+		background: radial-gradient(circle, rgba(63, 94, 251, 1) 0%, rgba(252, 70, 107, 1) 100%);
+	}
+	h1 {
+		color: red;
+		font-size: 75px;
+	}
+	.square {
+		position: absolute;
+		bottom: 1rem;
+		left: 1rem;
+		display: flex;
+		background: black;
+		color: white;
+		padding: 2rem;
+		font-size: 50px;
+	}
 </style>
-
