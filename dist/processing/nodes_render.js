b98d49ded98a5643702abf226d4d9660aa9f8bb0
Stephen Gunn <stephengunn@gmail.com>
2024-11-08 21:26:43 -0600

    juiced

--- dist/processing/nodes_render.js
+++ dist/processing/nodes_render.js
@@ -1,7 +1,6 @@
 // Take a svelte component, render it down to html, inline the styles
 // then return a set of satori ready nodes
-import pkg from 'juice';
-const { inlineContent } = pkg;
+import juice from 'juice';
 import { html as to_satori_nodes } from 'satori-html';
 import { render } from 'svelte/server';
 export const nodes_render = async (Component, props) => {
@@ -12,7 +11,7 @@ export const nodes_render = async (Component, props) => {
     }
     console.log('head', head);
     console.log('body', body);
-    const inline_html = inlineContent(body, head, {});
+    const inline_html = juice(head + body, {});
     console.log('inline_html', inline_html);
     // render satori friendly HTML and return it
     const satori_nodes = to_satori_nodes(inline_html);
--- src/lib/processing/nodes_render.ts
+++ src/lib/processing/nodes_render.ts
@@ -1,36 +1,35 @@
 // Take a svelte component, render it down to html, inline the styles
 // then return a set of satori ready nodes
-import pkg from 'juice';
-const { inlineContent } = pkg;
+import juice from 'juice';
 import { html as to_satori_nodes } from 'satori-html';
 import { render } from 'svelte/server';
 
 export const nodes_render = async (
-	Component: any,
-	props?: {
-		[key: string]: any;
-	}
+  Component: any,
+  props?: {
+    [key: string]: any;
+  }
 ) => {
-	// render the body and the head
-	const { head, body } = render(Component, { props });
+  // render the body and the head
+  const { head, body } = render(Component, { props });
 
-	if (!head) {
-		console.error(
-			'CSS not being returned from the Svelte component. Please add <svelte:options css="injected" /> to the top of your image component.'
-		);
-	}
+  if (!head) {
+    console.error(
+      'CSS not being returned from the Svelte component. Please add <svelte:options css="injected" /> to the top of your image component.'
+    );
+  }
 
-	console.log('head', head);
-	console.log('body', body);
+  console.log('head', head);
+  console.log('body', body);
 
-	const inline_html: string = inlineContent(body, head, {});
+  const inline_html: string = juice(head + body, {});
 
-	console.log('inline_html', inline_html);
+  console.log('inline_html', inline_html);
 
-	// render satori friendly HTML and return it
-	const satori_nodes = to_satori_nodes(inline_html);
+  // render satori friendly HTML and return it
+  const satori_nodes = to_satori_nodes(inline_html);
 
-	console.log('satori_nodes', satori_nodes);
+  console.log('satori_nodes', satori_nodes);
 
-	return satori_nodes;
+  return satori_nodes;
 };
